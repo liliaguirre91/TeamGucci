1a4a793c979ee88636a6ba4fd6cf291e4c0fa9a7
liliaguirre91 <lili91@nmsu.edu>
2019-09-25 19:34:26 -0600

    Lightweight UI app using React

---
+++ lightweight/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+         First name: <br/>
+          <input type="text" value={this.state.value} onChange={this.handleChange} /> <br/>
+          
+         Last name: <br/>
+          <input type="text" value={this.state.value} onChange={this.handleChange} /> <br/><br/>
+          
+          
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root')
+);
+
+export default NameForm;
