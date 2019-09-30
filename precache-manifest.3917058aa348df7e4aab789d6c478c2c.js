74ca9474a51ec38231622ac7121cf967fcb1eb78
liliaguirre91 <lili91@nmsu.edu>
2019-09-30 12:34:08 -0600

    Updates

---
+++ precache-manifest.3917058aa348df7e4aab789d6c478c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918e5b5e12fbde750415b5843862c000",
+    "url": "/lightweight/index.html"
+  },
+  {
+    "revision": "95aba56e8f533501ca79",
+    "url": "/lightweight/static/css/main.b70bdeb2.chunk.css"
+  },
+  {
+    "revision": "916466bfeff744a8d207",
+    "url": "/lightweight/static/js/2.95033b10.chunk.js"
+  },
+  {
+    "revision": "95aba56e8f533501ca79",
+    "url": "/lightweight/static/js/main.d82f1b89.chunk.js"
+  },
+  {
+    "revision": "7eab5c7dd217c6a36ce4",
+    "url": "/lightweight/static/js/runtime-main.8efbff9b.js"
+  }
+]);
