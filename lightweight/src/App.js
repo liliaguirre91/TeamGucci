6a6e521f0d2a0a54024133a5dcea5bd795616527
Lucas <ljherrm@nmsu.edu>
2019-10-02 09:58:17 -0600

    Basic connectivity from React to API

--- lightweight/src/App.js
+++ lightweight/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NameForm from './component/NameForm';
 
-function App() {
-   return (
-      <div className="container">
-         <Router>
-            <div className="col-md-6">
-               <h1 className="text-center" style={style}>Lightweight Prototype</h1>
-               <Switch>
-                  <Route path="/" exact component={NameForm}/>
-               </Switch>
-            </div>
-         </Router>
+class App extends Component {
+  state = {
+    isLoading: true,
+    users: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/users');
+    const body = await response.json();
+    this.setState({ users: body, isLoading: false });
+  }
+
+  render() {
+    const {users, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>User List</h2>
+            {users.map(user =>
+              <div key={user.name}>
+                {user.name}
+                
+              </div>
+            )}
+          </div>
+          <div className="App-intro">
+            <h2>Email List</h2>
+            {users.map(user =>
+              <div key={user.email}>
+                {user.email}
+                
+              </div>
+              
+              
+            )}
+          </div>
+        </header>
       </div>
-   );
-}
-
-const style = {
-   color: 'red',
-   margin: '10px'
+    );
+  }
 }
 
 export default App;
