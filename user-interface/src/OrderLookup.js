0876c0de008fb972c4a749c5b394f43812230ea7
Lucas <ljherrm@nmsu.edu>
2019-11-14 11:22:29 -0700

    "Finished" order lookup page

--- user-interface/src/OrderLookup.js
+++ user-interface/src/OrderLookup.js
@@ -1,9 +1,9 @@
 //import React from 'react';
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-import { lookupOrder } from './util/APIFunctions';
+import { lookupOrder, getProductsOrdered } from './util/APIFunctions';
 import './OrderLookup.css';
-import { Form, Input, Button, message } from 'antd'
+import { Form, Input, Button, message, Table, notification } from 'antd'
 const FormItem= Form.Item;
 
 class OrderLookup extends Component {
@@ -13,7 +13,8 @@ class OrderLookup extends Component {
          OrderID: '',
          result: '',
          deliveryInfo:'',
-         submitted: false
+         submitted: false,
+         productsOrdered: []
       };
       this.handleIDChange = this.handleIDChange.bind(this);    
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -34,12 +35,26 @@ class OrderLookup extends Component {
       
       const orderNumber = this.state.OrderID;
       
+      //lookup order number and return if delivered or not
       await lookupOrder(orderNumber) 
       .then(result => 
          this.setState({ result })
        );
       
-            
+       //get the products ordered attached to the order number
+       await getProductsOrdered(orderNumber)
+       .then (response => {
+          this.setState({
+            productsOrdered: response
+          })
+       })
+       .catch(error => {
+         notification.error({
+             message: '',
+             description: error.message
+         });
+     })
+
       /*setTimeout(function() {
          if (this.state.result == 'false')
             alert('Your product has not been delivered!!');
@@ -50,37 +65,53 @@ class OrderLookup extends Component {
          this.setState({ submitted: true });
       }.bind(this), 200)
       */
-      
+         //check if order has been delivered or not, give a message saying yes or no
          if (this.state.result == 'false') {
-            //this.setState({ deliveryInfo: 'Your product has not been delivered yet'});
-            //alert('Your product has not been delivered yet');
+            
             message.error('Order number ' + orderNumber + ' has not been delivered yet.');
             this.setState({deliveryInfo:"Your Order has not been delivered yet."});
+            
          }
          else if (this.state.result == 'true') {
-            //this.setState({ deliveryInfo: 'Your product has been delivered!!' });
-            //alert('Your product has been delivered!');
+            
             message.success('Order number ' + orderNumber + ' has been delivered!')
             this.setState({deliveryInfo: "Your order has been delivered!!"});
+           
          }
 
-         //never triggers because of the await on line 37
+         //order could not be found
+         //never triggers because of the await on line 38
         else {
-         //this.setState({ deliveryInfo: this.state.result });
+         
          alert('Could not find order');
-         this.setState({deliveryInfo:"There is not order with that order number!!"});
+         this.setState({deliveryInfo:"There is no order with that order number!!"});
+         
         }
       
-         
          this.setState({ submitted: true});
-      }
-   
-
-   
-
-  
+      }  
    
    render() {
+      
+      //table columns
+      const columns = [
+         {
+            title: 'Order Number',
+            dataIndex: 'order_id',
+            key: 'order_id'
+         },
+         {
+            title:'Products',
+            dataIndex: 'product_id',
+            key: 'product_id'
+         },
+         {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            key: 'quantity'
+         }
+      ];
+      
       return (
          <div className="order-search-container">
             <h1 className="page-title">Order Lookup</h1>
@@ -106,6 +137,7 @@ class OrderLookup extends Component {
                 </Form>
                 {this.state.submitted &&
                   <h1>{this.state.deliveryInfo}</h1>}
+                  <Table columns = {columns} dataSource = {this.state.productsOrdered} pagination = {false} size = 'middle'/>
          </div>
       );
    }
