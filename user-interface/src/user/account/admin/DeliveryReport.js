bef399df38d7bc1f04f9324115c270785ad25eec
liliaguirre91 <lili91@nmsu.edu>
2019-12-04 19:31:57 -0700

    Cleaning up CreateAdmin and DeliveryReport

--- user-interface/src/user/account/admin/DeliveryReport.js
+++ user-interface/src/user/account/admin/DeliveryReport.js
@@ -1,28 +1,12 @@
-//import React from 'react';
-import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import {CSVLink, CSVDownload} from 'react-csv';
 import './DeliveryReport.css';
 import { 
-    getOrdersNotDelivered, 
-    getProductsOrdered, 
-    getOrdersDelivered,
-    getProduct, 
-    getProducts, 
+    getOrdersNotDelivered, getProductsOrdered, 
+    getOrdersDelivered,getProducts, 
     setToDelivered } from '../../../util/APIFunctions';
 
-import {
-    Form, 
-    Input, 
-    Button, 
-    notification, 
-    Table, 
-    Badge, 
-    Menu, 
-    Dropdown, 
-    Icon,
-    Row,
-    Col } from 'antd';
+import { Button, notification, Table, Row, Col } from 'antd';
 
 
 class DeliveryReport extends Component {
@@ -44,15 +28,11 @@ class DeliveryReport extends Component {
     
 /*******************************************************************************************/  
     async componentDidMount() {
-        console.log( this.state.toggle );
-        var productQuantities= [];
-        var product= [];
         const allProducts = [];
         var data = [];
         const products = []
         let setCampaign = JSON.parse(localStorage.getItem('setCampaign'));
         let campaignYear = setCampaign["year"];
-        //var orders = [];
         const response = await getProducts(campaignYear)
             .then( response => {
                 this.setState( {
@@ -65,80 +45,28 @@ class DeliveryReport extends Component {
                     description: error.message || 'Sorry! Something went wrong!'
                 });
             })
-        const idk = this.state.productTmp;
-        //console.log( idk );
-        for( var i = 0; i < idk.length; i++ ){
-            allProducts[ idk[i].productId ] = idk[i].product
-        }
-        console.log( allProducts );
+        const tempProd = this.state.productTmp;
         
-        //console.log("hello" + campaignYear);
-        if( this.state.toggle === 1 ){
-        const response = await getOrdersNotDelivered(campaignYear)
-            .then (response => {
-                this.setState({
-                    orders: response
-                });
-                const orders = this.state.orders;
-                for (var i = 0; i < orders.length; i++) {
-                    //console.log("total cost:" + orders[i].totalCost)
-                    if (orders[i].totalCost != undefined) 
-                        this.state.orders[i].totalCost = "$" + orders[i].totalCost.toString() + ".00";
-                    if (orders[i].paid != undefined)
-                        this.state.orders[i].paid = "$" + orders[i].paid.toString() + ".00";
-                    
-                    //console.log("total cost:" + this.state.orders[i].totalCost);
-                }
-                //console.log("Orders:" + JSON.stringify(this.state.orders));
-            })
-            .catch(error => {
-                notification.error({
-                    message: 'LCHS Band Fundraising',
-                    description:error.message || 'Sorry! Something went wrong!'
-                });
-            })
+        for( var i = 0; i < tempProd.length; i++ ){
+            allProducts[ tempProd[i].productId ] = tempProd[i].product
         }
-        else if( this.state.toggle === 2 ){
-            const response = await getOrdersDelivered(campaignYear)
+        
+        if( this.state.toggle === 1 ) {
+            
+            await getOrdersNotDelivered(campaignYear)
                 .then (response => {
                     this.setState({
                         orders: response
                     });
+                    
                     const orders = this.state.orders;
+                    
                     for (var i = 0; i < orders.length; i++) {
-                        //console.log("total cost:" + orders[i].totalCost)
                         if (orders[i].totalCost != undefined) 
                             this.state.orders[i].totalCost = "$" + orders[i].totalCost.toString() + ".00";
                         if (orders[i].paid != undefined)
                             this.state.orders[i].paid = "$" + orders[i].paid.toString() + ".00";
-                        
-                        //console.log("total cost:" + this.state.orders[i].totalCost);
                     }
-                    //console.log("Orders:" + JSON.stringify(this.state.orders));
-                })
-                .catch(error => {
-                    notification.error({
-                        message: 'LCHS Band Fundraising',
-                        description:error.message || 'Sorry! Something went wrong!'
-                    });
-                })
-            }
-        const orders = this.state.orders;
-        //console.log(orders[0].orderId);
-        for (var i = 0; i < orders.length; i++) {
-            const productResponse = await getProductsOrdered(orders[i].orderId) 
-                .then (productResponse => {
-                    const length = productResponse.length;
-                    for (var i = 0; i < length; i++) {
-                        //do the api call to get product with productid
-                        const item = { productID: productResponse[i].productId, name: allProducts[productResponse[i].productId], quantity: productResponse[i].quantity };
-                        //console.log(orders[i].orderId, item);
-                        data.push(item);
-                        console.log(data);
-
-                    }
-                     products.push(data);
-                     data = [];
                 })
                 .catch(error => {
                     notification.error({
@@ -147,21 +75,64 @@ class DeliveryReport extends Component {
                     });
                 })
         }
-       
-        //console.log(data);
+        else if( this.state.toggle === 2 ) {
+            
+            await getOrdersDelivered(campaignYear)
+                .then (response => {
+                    this.setState({
+                        orders: response
+                    });
+                    
+                    const orders = this.state.orders;
+                    
+                    for (var i = 0; i < orders.length; i++) {
+                        if (orders[i].totalCost != undefined) 
+                            this.state.orders[i].totalCost = "$" + orders[i].totalCost.toString() + ".00";
+                        if (orders[i].paid != undefined)
+                            this.state.orders[i].paid = "$" + orders[i].paid.toString() + ".00";
+                    }
+                })
+                .catch(error => {
+                    notification.error({
+                        message: 'LCHS Band Fundraising',
+                        description:error.message || 'Sorry! Something went wrong!'
+                    });
+                })
+        }
+        
+        const orders = this.state.orders;
+
+        for (var i = 0; i < orders.length; i++) {
+            await getProductsOrdered(orders[i].orderId) 
+                .then (productResponse => {
+                    const length = productResponse.length;
+                    
+                    for (var i = 0; i < length; i++) {
+                        //do the api call to get product with productid
+                        const item = { productID: productResponse[i].productId, name: allProducts[productResponse[i].productId], quantity: productResponse[i].quantity };
+                        data.push(item);
+                    }
+                    
+                    products.push(data);
+                    data = [];
+                })
+                .catch(error => {
+                    notification.error({
+                        message: 'LCHS Band Fundraising',
+                        description:error.message || 'Sorry! Something went wrong!'
+                    });
+                })
+        }
         this.setState({items: products});
-        //console.log(this.state.items);
     }
 
 /*******************************************************************************************/    
     start = () => {
         const success = true;
-        console.log(this.state.loading);
         this.setState({ loading: true });
         
         const selectedRowKeys = this.state.selectedRowKeys;
-        console.log(this.state.selectedRowKeys);
-        if( this.state.toggle === 1 ){
+        if( this.state.toggle === 1 ) {
             for (var i = 0; i < selectedRowKeys.length; i++) {
                 setToDelivered(selectedRowKeys[i], true) 
                     .catch(error => {
@@ -174,6 +145,7 @@ class DeliveryReport extends Component {
                 if (!success)
                     break;
             }//End for
+            
             if (success) {
                 window.location.reload();
                 notification.success({
@@ -195,6 +167,7 @@ class DeliveryReport extends Component {
                 if (!success)
                     break;
             }//End for
+            
             if (success) {
                 window.location.reload();
                 notification.success({
@@ -204,19 +177,17 @@ class DeliveryReport extends Component {
             }
         }
         
-        
         setTimeout(() => {
-        this.setState({
-            selectedRowKeys: [],
-            loading: false,
-        });
+            this.setState({
+                selectedRowKeys: [],
+                loading: false,
+            });
         }, 1000);
     };
 
   
 /*******************************************************************************************/    
     onSelectChange = selectedRowKeys => {
-        console.log('selectedRowKeys changed: ', selectedRowKeys);
         this.setState({ selectedRowKeys });
     };
 
@@ -236,13 +207,10 @@ class DeliveryReport extends Component {
   
 /*******************************************************************************************/  
     getOrderedProducts(x)  {
-        console.log(x);
-        const orders = this.state.orders;
-        
         const columns = [
-        { title: 'Product ID', dataIndex: 'productID', key: 'productID' },
-        { title: 'Product Name', dataIndex: 'name', key: 'name' },
-        { title: 'Quantity', dataIndex: 'quantity', key: 'quantity' },
+            { title: 'Product ID', dataIndex: 'productID', key: 'productID' },
+            { title: 'Product Name', dataIndex: 'name', key: 'name' },
+            { title: 'Quantity', dataIndex: 'quantity', key: 'quantity' },
         ];
 
         const products = this.state.items;
@@ -297,21 +265,19 @@ class DeliveryReport extends Component {
             selectedRowKeys,
             onChange: this.onSelectChange,
         };
-        console.log(selectedRowKeys);
+        
         const hasSelected = selectedRowKeys.length > 0;
         var subheading 
-        if( this.state.toggle === 1 ){
+        if( this.state.toggle === 1 ) {
             subheading = "Orders NOT Delivered";
         }
-        else if( this.state.toggle === 2 ){
+        else if( this.state.toggle === 2 ) {
             subheading = "Orders Delivered";
         }
-        const newline = "";
         
         return (
             <div className="delivery-report-container">
                 <h2 className="page-title">Delivery Report</h2>
-                
                 <Row gutter={[110]} type = 'flex'>
                     <Col span={7}>
                         <Button type="primary" onClick={this.start} disabled={!hasSelected} loading={loading}>
@@ -340,19 +306,15 @@ class DeliveryReport extends Component {
                         rowSelection={rowSelection}
                         expandedRowRender={(record, i) =>
                             this.getOrderedProducts(i)}
-                            
                         bordered
                         pagination={false}
                         scroll={{ y: 500 }}
                     />
                 </div>
-                
-            <CSVLink data={this.state.orders} >Download me</CSVLink>
+                <CSVLink data={this.state.orders} >Download me</CSVLink>
             </div>
         );
     }
 }
-
-
 
 export default DeliveryReport;
